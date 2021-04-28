231928dcc9ea079ab11dc7f7f1eabfb75723f6e5
Gabriela de Mesquita Sampaio Duarte <gabrielad4@al.insper.edu.br>
2021-04-28 10:46:53 -0300

    função que extrai o valor da carta.

--- funcoes.py
+++ funcoes.py
@@ -4,8 +4,17 @@ def cria_baralho():
     baralho = ['A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠','A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣']
     random.shuffle(baralho)
     return baralho
-    
+
 # extrai o naipe de uma carta quando a função recebe essa carta
 def extrai_naipe(string):
     naipe = string[-1]
     return naipe
+
+    # extrai o valor de uma carta quando a função recebe essa carta
+def extrai_valor(string):
+    if string[1] == '0':
+        valor = string[0:2]
+        return valor
+    else:
+        valor = string[0]
+        return valor
