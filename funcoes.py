022f0e0f354c7058a394e44ad28e60830626390b
Gabriela de Mesquita Sampaio Duarte <gabrielad4@al.insper.edu.br>
2021-04-28 10:49:23 -0300

    primeira versão da função que define os movimentos possíveis para cada carta do baralho.

--- funcoes.py
+++ funcoes.py
@@ -18,3 +18,24 @@ def extrai_valor(string):
     else:
         valor = string[0]
         return valor
+
+        # vendo as possíveis posições de uma carta dentro de uma lista de cartas durante o jogo   
+def lista_movimentos_possiveis(baralho, i):
+    if i == 0:
+        return []
+    if i>0 and i<3:
+        if (extrai_valor(baralho[i]) == extrai_valor(baralho[i-1])) or (extrai_naipe(baralho[i]) == extrai_naipe(baralho[i-1])):
+            return [1] 
+        else:
+            return []
+    if i >= 3:
+
+        if ((extrai_valor(baralho[i]) == extrai_valor(baralho[i-1])) or (extrai_naipe(baralho[i]) == extrai_naipe(baralho[i-1]))) and ((extrai_valor(baralho[i]) == extrai_valor(baralho[i-3])) or (extrai_naipe(baralho[i])== extrai_naipe(baralho[i-3]))):
+            return [1, 3]
+        elif (extrai_valor(baralho[i]) == extrai_valor(baralho[i-1])) or (extrai_naipe(baralho[i]) == extrai_naipe(baralho[i-1])):
+            return [1]
+        elif (extrai_valor(baralho[i]) == extrai_valor(baralho[i-3])) or (extrai_naipe(baralho[i])== extrai_naipe(baralho[i-3])):
+            return [3]
+        
+        else:
+            return []
