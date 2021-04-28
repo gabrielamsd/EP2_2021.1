849f20a8e3285db62c892e07f385d0e5d6e52f87
Gabriela de Mesquita Sampaio Duarte <gabrielad4@al.insper.edu.br>
2021-04-28 10:46:05 -0300

    implementando a função que cria um baralho.

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,10 @@
+# implementando o número de cartas de um baralho completo e embaralhando elas
+import random
+def cria_baralho():
+    baralho = ['A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠','A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣']
+    random.shuffle(baralho)
+    return baralho
+    
 # extrai o naipe de uma carta quando a função recebe essa carta
 def extrai_naipe(string):
     naipe = string[-1]
